fb6159dd1d457c70f90a644fa3d18143fbfbbba1
fajarmaul <of.maulana@gmail.com>
2017-09-05 23:53:45 +0700

    YayasanPage Login Fix

--- src/pages/login-yayasan/login-yayasan.ts
+++ src/pages/login-yayasan/login-yayasan.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { TabsYayasanPage } from '../tabs-yayasan/tabs-yayasan';
 import { RegisterYayasanPage } from '../register-yayasan/register-yayasan';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 
 /**
@@ -16,20 +17,39 @@ import { RegisterYayasanPage } from '../register-yayasan/register-yayasan';
   templateUrl: 'login-yayasan.html',
 })
 export class LoginYayasanPage {
+  @ViewChild('email') email;
+  @ViewChild('password') password;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, private fire:AngularFireAuth) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginYayasanPage');
   }
 
+    alert(message: string) {
+    this.alertCtrl.create({
+      title: 'Info!',
+      subTitle: message,
+      buttons: ['OK']
+    }).present();
+  }
+
   login(){
 
 	//this.navCtrl.setRoot(TabsPage);
-		this.navCtrl.push(TabsYayasanPage);
-
+    this.fire.auth.signInWithEmailAndPassword(this.email.value, this.password.value)
+    .then( data => {
+      this.alert('Success! you\'re logged in');
+      this.navCtrl.setRoot(TabsYayasanPage);
+    })
+    .catch( error => {
+      console.log('got an error in login-yayasan page', error);
+      this.alert(error.message);
+    })
+    console.log('Would sign in with', this.email.email, this.password.value);
 	}
+
 daftar(){
 
   //this.navCtrl.setRoot(TabsPage);
